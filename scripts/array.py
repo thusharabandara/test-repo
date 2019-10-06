b7557acecd719fee3e0ecf6c44f24b592012c949
thusharabandara <thushararanjana@gmail.com>
2019-10-06 10:54:20 +0530

    add two arrays

--- scripts/array.py
+++ scripts/array.py
@@ -2,7 +2,11 @@
 import numpy as np 
 
 # create an array
-arr = np.array([1, 2, 3, 4, 5])
+arr1 = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([10, 11, 12, 13, 14])
+
+# get the total of two arrays
+arr = arr1 + arr2
 
 # display the array
 print(arr)
